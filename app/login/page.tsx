a5c19eadeaa535bb111169c626c24480d9031d8c
Kentai Radiquum <kentai.waah@gmail.com>
2024-08-04 14:53:19 +0500

    fix: ```useSearchParams() should be wrapped in a suspense boundary at page "/login".```

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,9 @@
+import dynamic from "next/dynamic";
 import { LoginPage } from "#/pages/Login";
+
+const LoginDynamic = dynamic(() => Promise.resolve(LoginPage), {
+  ssr: false,
+});
 export default function Login() {
-  return <LoginPage />;
+  return <LoginDynamic />;
 }
